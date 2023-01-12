ee8d04efbc23fa85482b34085afc202f361d5539
agustinrobledo <agustinrobledo59.ar@gmail.com>
2023-01-12 14:12:45 -0300

    empty favorite coins screen

--- src/components/favorites/FavoriteDetailScreen.tsx
+++ src/components/favorites/FavoriteDetailScreen.tsx
@@ -1,10 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 export default function FavoriteDetailScreen() {
   return (
-    <View>
-      <Text>FavoriteDetailScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.emptyText}>
+        You don't have any favorite coin yet.
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent: "center",
+    paddingTop: 20,
+  },
+  emptyText: {
+    alignSelf: "center",
+    fontSize: 15,
+  },
+});
