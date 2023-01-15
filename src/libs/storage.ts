414bbc32cebc74e98ab54fa76cc6bdc5a6917599
agustinrobledo <agustinrobledo59.ar@gmail.com>
2023-01-15 16:40:01 -0300

    lib of storage

---
+++ src/libs/storage.ts
@@ -0,0 +1,52 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export default class Storage {
+  static instance = new Storage();
+
+  store = async (key: string, value: string) => {
+    try {
+      await AsyncStorage.setItem(key, value);
+
+      return true;
+    } catch (error) {
+      console.log(error);
+
+      return false;
+    }
+  };
+
+  get = async (key: string) => {
+    try {
+      return await AsyncStorage.getItem(key);
+    } catch (error: any) {
+      console.log(error);
+      throw Error(error);
+    }
+  };
+
+  multiGet = async (keys: string[]) => {
+    try {
+      return await AsyncStorage.multiGet(keys);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getAllKeys = async () => {
+    try {
+      return await AsyncStorage.getAllKeys();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  remove = async (key: string) => {
+    try {
+      await AsyncStorage.removeItem(key);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+}
